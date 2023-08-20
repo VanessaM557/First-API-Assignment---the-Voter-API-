a9d9cadae41eb8b817b16f30229f12ffb5a7d83c
VanessaM557 <122654057+VanessaM557@users.noreply.github.com>
2023-08-20 15:44:25 -0400

    Update voterLogic.go
    Internet issues so want to commit before resuming

--- voterLogic.go
+++ voterLogic.go
@@ -1,1 +1,17 @@
+package main
+import (
+	"fmt"
+	"time"
+)
+
+// Function to create a new unique voter 
+
+func CreateNewVoter(voterID uint, firstName, lastName string) Voter {
+	return Voter{
+		VoterID:     voterID,
+		FirstName:   firstName,
+		LastName:    lastName,
+		VoteHistory: make([]voterPoll, 0),
+	}
+}
 
