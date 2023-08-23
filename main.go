9349377c9896f918f5bcf10ca8d0c29166028afc
VanessaM557 <122654057+VanessaM557@users.noreply.github.com>
2023-08-23 19:49:48 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,70 +1,42 @@
 package main
-
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
+	"strings"
+	"github.com/go-redis/redis/v8" 
 )
 
-func main() {
-	// Initializing the VoterList
-	voterList := NewVoterList()
+var context = context.Background()
+var rdb *redis.Client
 
-	//Switch to choose between methods of GET and POST
-	
+//Vote struct 
+
+type Voter struct {
+	ID   string
+	Name string
+}
+
+func main() {
+	rdb = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		DB:   0,
+	})
+
 	http.HandleFunc("/voters", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
-			handleGetAllVoters(w, r, voterList)
+			handleGetAllVoters(w, r)
 		case http.MethodPost:
-			handleCreateNewVoter(w, r, voterList)
+			handleCreateNewVoter(w, r)
 		default:
-			http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
+			http.Error(w, "Method is not permmitted", http.StatusMethodNotAllowed)
 		}
 	})
 
 	http.HandleFunc("/voters/", func(w http.ResponseWriter, r *http.Request) {
-		handleGetVoterByID(w, r, voterList)
-	})
-
-	http.HandleFunc("/voters/:id/polls", func(w http.ResponseWriter, r *http.Request) {
-		handleGetVoterPollsByID(w, r, voterList)
-	})
-
-	http.HandleFunc("/voters/:id/polls/:pollid", func(w http.ResponseWriter, r *http.Request) {
-		handleGetVoterPollByID(w, r, voterList)
-	})
-
-	//incorporating extra credit of PUT/DELETE
-	
-	http.HandleFunc("/voters/:id", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			handleGetVoterByID(w, r, voterList)
-		case http.MethodPut:
-			handleUpdateVoterByID(w, r, voterList)
-		case http.MethodDelete:
-			handleDeleteVoterByID(w, r, voterList)
-		default:
-			http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
-		}
-	})
-
-	http.HandleFunc("/voters/:id/polls/:pollid", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodGet:
-			handleGetVoterPollByID(w, r, voterList)
-		case http.MethodPut:
-			handleUpdateVoterPollByID(w, r, voterList)
-		case http.MethodDelete:
-			handleDeleteVoterPollByID(w, r, voterList)
-		default:
-			http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
-		}
-	})
-
-	http.HandleFunc("/voters/health", func(w http.ResponseWriter, r *http.Request) {
-		handleHealthCheck(w)
+		handleGetVoterByID(w, r)
 	})
 
 	port := 8080
@@ -72,3 +44,76 @@ func main() {
 	http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
 
+func handleGetAllVoters(w http.ResponseWriter, r *http.Request) {
+	voterKeys, err := rdb.Keys(context, "voter:*").Result()
+	if err != nil {
+		http.Error(w, "Error retrieving voters", http.StatusInternalServerError)
+		return
+	}
+
+	voters := []Voter{}
+	for _, key := range voterKeys {
+		voterJSON, err := rdb.Get(context, key).Result()
+		if err != nil {
+			http.Error(w, "Error retrieving voter", http.StatusInternalServerError)
+			return
+		}
+		var voter Voter
+		err = json.Unmarshal([]byte(voterJSON), &voter)
+		if err != nil {
+			http.Error(w, "Error decoding voter", http.StatusInternalServerError)
+			return
+		}
+		voters = append(voters, voter)
+	}
+
+	responseJSON, err := json.Marshal(voters)
+	if err != nil {
+		http.Error(w, "Error encoding response", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseJSON)
+}
+
+func handleCreateNewVoter(w http.ResponseWriter, r *http.Request) {
+	var voter Voter
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&voter)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	voterJSON, err := json.Marshal(voter)
+	if err != nil {
+		http.Error(w, "Error encoding voter", http.StatusInternalServerError)
+		return
+	}
+
+	err = rdb.Set(context, "voter:"+voter.ID, voterJSON, 0).Err()
+	if err != nil {
+		http.Error(w, "Error saving voter", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(voterJSON)
+}
+
+func handleGetVoterByID(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/voters/")
+
+	voterJSON, err := rdb.Get(context, "voter:"+id).Result()
+	if err == redis.Nil {
+		http.Error(w, "Voter cannot be found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		http.Error(w, "Error retrieving voter", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(voterJSON))
+}
